ec1bf5de72e6587061cde553fb088f26e6e2182c
mujeeb <md.mujeeb5577@gmail.com>
2025-12-09 13:27:28 +0530

    removed address

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,14 +14,6 @@ const Contact = () => {
         <span className="text-amber-400">Get</span> in Touch
       </motion.h1>
       <div className="text-center tracking-tight">
-        <motion.p
-          whileInView={{ opacity: 1, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
-          transition={{ duration: 1 }}
-          className="my-4"
-        >
-          {CONTACT.address}
-        </motion.p>
         {/* <motion.p
           whileInView={{ opacity: 1, x: 0 }}
           initial={{ opacity: 0, x: 100 }}
